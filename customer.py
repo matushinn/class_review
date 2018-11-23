82fe1cfb09b3e230c449aed8784d30f4211fd373
shotaro-ooe <shoooe97@gmail.com>
2018-11-23 17:29:08 +0900

    B-3 終了

--- customer.py
+++ customer.py
@@ -8,6 +8,20 @@ class Customer:
     def full_name(self):
         print(f"{self.first_name} {self.family_name}")
 
+    def entry_fee(self):
+        if self.age > 65:
+            amount = 1200
+            print(amount)
+
+        if self.age >= 20 and self.age < 65:
+            amount = 1500
+            print(amount)
+
+        if self.age < 20:
+            amount = 1000
+            print(amount)
+
+
 #ken = Customer(first_name="Ken", family_name="Tanaka")
 #ken.full_name()  # "Ken Tanaka" という値を返す
 
