d713e81f7d739cf7bda05039213be735f3ec346b
shotaro-ooe <shoooe97@gmail.com>
2018-11-23 17:22:43 +0900

    B-2 終了

--- customer.py
+++ customer.py
@@ -1,15 +1,16 @@
 class Customer:
-    def __init__(self,first_name,family_name):
+    def __init__(self,first_name,family_name,age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
 
     def full_name(self):
         print(f"{self.first_name} {self.family_name}")
 
-ken = Customer(first_name="Ken", family_name="Tanaka")
-ken.full_name()  # "Ken Tanaka" という値を返す
+#ken = Customer(first_name="Ken", family_name="Tanaka")
+#ken.full_name()  # "Ken Tanaka" という値を返す
 
-tom = Customer(first_name="Tom", family_name="Ford")
-tom.full_name()  # "Tom Ford" という値を返す
+#tom = Customer(first_name="Tom", family_name="Ford")
+#tom.full_name()  # "Tom Ford" という値を返す
 
